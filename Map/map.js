88c7b37dcb41614872e4b0a9036a1f50c1eba40c
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2022-08-23 21:12:37 +0200

    First home made polygone function.

---
+++ Map/map.js
@@ -0,0 +1,12 @@
+function onMapClick(e) {
+    //polygon.remove();
+    pol.push([e.latlng.lat,e.latlng.lng])
+    textbx = document.getElementById('OutputBox');
+    //textbx.value +=  e.latlng + "\n";
+    textbx.value = pol
+    polygon.setLatLngs(pol);
+}
+
+//latlng: v
+//lat: 50.15050636899788
+//lng: 31.129760742187504

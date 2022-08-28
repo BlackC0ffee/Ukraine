8f5353640678681e413ccb82f840545a5bd5308c
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2022-08-28 20:42:46 +0200

    added list of polygons

--- Map/map.js
+++ Map/map.js
@@ -1,15 +1,16 @@
-function Polygon(Cordinates, Color) {
-    this.Cordinates = Cordinates;
-    this.Color = Color;
-}
-
 function onMapClick(e) {
     //polygon.remove();
     pol.push([e.latlng.lat,e.latlng.lng])
     textbx = document.getElementById('OutputBox');
     //textbx.value +=  e.latlng + "\n";
     textbx.value = pol
-    polygon.setLatLngs(pol);
+    activePolygon.polygon.setLatLngs(pol);
+}
+
+function newPolygon(name, color){
+    listOfPolygons.push(activePolygon);
+    pol = new Array();
+    activePolygon = { name: name, polygon: L.polygon(pol, {color: color}).addTo(map) }
 }
 
 //latlng: v

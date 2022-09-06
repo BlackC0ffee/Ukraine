698777d1c23ffafdd6513acc4e16d7f93ff59933
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2022-09-06 08:46:17 +0200

    EventListner + PlygonValue based on colorname option

--- Map/map.js
+++ Map/map.js
@@ -7,15 +7,31 @@ function onMapClick(e) {
 }
 
 function newPolygon(name, color){
-    listOfPolygons.push(activePolygon);
+    //if (activePolygon != null) { listOfPolygons.push(activePolygon); }
+    
     pol = new Array();
     activePolygon = { name: name, polygon: L.polygon(pol, {color: color}).addTo(map) }
+    listOfPolygons.push(activePolygon);
+    BuildPolygonList();
 
-    opt = document.createElement("option");
-    opt.text = name;
-    opt.value = name;
-    document.getElementById('PolygonsList').options.add(opt);
+    //document.getElementById('PolygonsList').options.add(opt);
     EnableCrosshair();
+}
+
+function BuildPolygonList(){
+    list = document.getElementById('PolygonsList');
+    for (i = list.length - 1; i >= 0; i--) {
+        list.remove(i);
+    }
+    //list.remove(list.selectedIndex);
+    //list.remove();
+
+    for (let index = 0; index <= listOfPolygons.length; index++) {
+        opt = document.createElement("option");
+        opt.text = listOfPolygons[index].name;
+        opt.value = listOfPolygons[index].name; //Index might be better
+        list.options.add(opt);
+    }
 }
 
 function EnableCrosshair(){
@@ -24,6 +40,18 @@ function EnableCrosshair(){
 
 function DisableCrosshair(){
     L.DomUtil.removeClass(map._container,'crosshair-cursor-enabled');
+}
+
+function makePolygonActive(e){
+    console.log(e.value);
+    for (let index = 0; index < listOfPolygons.length; index++) {
+        if(listOfPolygons[index].name == e.value){
+            console.log(pol);
+            pol = listOfPolygons[index].polygon._latlngs[0];
+            console.log(pol);
+            //activePolygon = { name: listOfPolygons[index].name, polygon: L.polygon(pol, {color: listOfPolygons[index].polygon.options.color}).addTo(map) }
+        }     
+    }
 }
 
 function convertToJson(listOfPolygons){
